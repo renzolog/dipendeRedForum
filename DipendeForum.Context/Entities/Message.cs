8e244e14217ac401de57f981f912ebfde8837a83
Massimiliano De Crignis <massimilianodecrignis@gmail.com>
2021-04-01 18:42:40 +0200

    Added entities and dbcontext

---
+++ DipendeForum.Context/Entities/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DipendeForum.Context.Entities
+{
+    public class Message
+    {
+
+    }
+}
---
+++ DipendeForum.Context/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DipendeForum.Context.Entities
+{
+    public class User
+    {
+        [Index(IsUnique = true)]
+        public string Username { get; set; }
+    }
+}
