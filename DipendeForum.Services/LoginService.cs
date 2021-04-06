47a8902b6a3b5d3ff540b92fb7745a79b8d047ea
Federico Lecca <federico.lecca@hotmail.com>
2021-04-06 22:24:31 +0200

    Added ILoginService and LoginService

---
+++ DipendeForum.Services/LoginService.cs
@@ -0,0 +1,11 @@
+﻿using DipendeForum.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DipendeForum.Services
+{
+    public class LoginService : ILoginService
+    {
+    }
+}
