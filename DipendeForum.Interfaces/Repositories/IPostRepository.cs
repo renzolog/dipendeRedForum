3ebc3b35d353803b68e9d8c744c51f74bda4f665
Massimiliano De Crignis <massimilianodecrignis@gmail.com>
2021-04-06 22:17:25 +0200

    added IMessageRepo, IPostRepo, MessageRepo and PostRepo

--- DipendeForum.Interfaces/Repositories/IPostRepository.cs
+++ DipendeForum.Interfaces/Repositories/IPostRepository.cs
@@ -1,12 +1,20 @@
 
 ﻿using System;
-using DipendeForum.Domain;
+ using System.Collections.Generic;
+ using DipendeForum.Domain;
 
 namespace DipendeForum.Interfaces.Repositories
 {
     public interface IPostRepository : ICrudRepository<PostDomain, Guid>
     {
-        
+        void Add(PostDomain post);
 
+        PostDomain GetById(Guid id);
+
+        ICollection<PostDomain> GetAll();
+
+        void Update(PostDomain message);
+
+        void Delete(PostDomain message);
     }
 }
--- DipendeForum.Repositories/MessageRepository.cs
+++ DipendeForum.Repositories/MessageRepository.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using AutoMapper;
+using DipendeForum.Context;
+using DipendeForum.Context.Entities;
+using DipendeForum.Domain;
+using DipendeForum.Interfaces.Repositories;
 
 namespace DipendeForum.Repositories
 {
-    public class MessageRepository
+    public class MessageRepository : IMessageRepository
     {
+        private readonly ForumDbContext _ctx;
+        private readonly IMapper _mapper;
+
+        public MessageRepository(ForumDbContext ctx, IMapper mapper)
+        {
+            _ctx = ctx;
+            _mapper = mapper;
+        }
+
+        public void Add(MessageDomain message)
+        {
+            //var messageEntity = _mapper.Map<Message>(message);
+            //_ctx.Message.Add(messageEntity);
+            //_ctx.SaveChanges();
+        }
+
+        public void Delete(MessageDomain element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<MessageDomain> GetAll()
+        {
+            //var messageEntities = _ctx.Message;
+            //var messageDomainList = _mapper.ProjectTo<MessageDomain>(messageEntities).ToList();
+            //return messageDomainList;
+            throw new NotImplementedException();
+        }
+
+        public MessageDomain GetById(Guid id)
+        {
+            //var message = _ctx.Message.FirstOrDefault(m => m.Id == id);
+            //var messageEntity = _mapper.Map<Message>(message);
+            //return messageEntity;
+            throw new NotImplementedException();
+        }
+
+        public void Update(MessageDomain element)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
