7a4c29445c6204de22adfb17bda4fa88b632cff8
Federico Lecca <federico.lecca@hotmail.com>
2021-04-06 22:12:31 +0200

    Added IUserRepository and UserRepository

---
+++ DipendeForum.Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DipendeForum.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+
+    }
+}
---
+++ DipendeForum.Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using DipendeForum.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DipendeForum.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+    }
+}
