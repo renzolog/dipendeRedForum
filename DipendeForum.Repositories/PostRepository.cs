c33ac7864bf5d52fc072e8c953a2486532dec233
Federico Lecca <federico.lecca@hotmail.com>
2021-04-08 12:52:48 +0200

    Implemented Post.GetById() - Tests still missing

--- DipendeForum.Repositories/PostRepository.cs
+++ DipendeForum.Repositories/PostRepository.cs
@@ -5,6 +5,8 @@ using AutoMapper;
 using DipendeForum.Context;
 using DipendeForum.Domain;
 using DipendeForum.Interfaces.Repositories;
+using System.Linq;
+using DipendeForum.Context.Entities;
 
 namespace DipendeForum.Repositories
 {
@@ -36,7 +38,13 @@ namespace DipendeForum.Repositories
 
         public PostDomain GetById(Guid id)
         {
-            throw new NotImplementedException();
+
+            Post postEntity = _ctx.Post
+                .FirstOrDefault(p => p.Id == id);
+
+            PostDomain postDomain = _mapper.Map<PostDomain>(postEntity);
+
+            return postDomain;
         }
 
         public void Update(PostDomain message)
